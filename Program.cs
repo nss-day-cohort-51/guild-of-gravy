ca04ffeb6770561bc256d97c267ec6eb02305d44
Brady Williams <williams.cole29@yahoo.com>
2021-11-22 10:21:50 -0600

    Finished Phase 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace plan_your_heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+
+            TeamMember member1 = new TeamMember();
+
+            Console.Write("Enter team member name: ");
+            member1.Name = Console.ReadLine();
+
+            Console.Write($"Enter {member1.Name}'s skill level: ");
+            member1.SkillLevel = int.Parse(Console.ReadLine());
+
+            Console.Write($"Enter {member1.Name}'s courage factor: ");
+            member1.CourageFactor = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($@"
+                Member Name: {member1.Name}
+                Member Skill Level: {member1.SkillLevel}
+                Member Courage Factor: {member1.CourageFactor}
+            ");
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace plan_your_heist
+{
+    class TeamMember
+    {
+        public string Name { get; set; }
+
+        public int SkillLevel { get; set; }
+
+        public decimal CourageFactor { get; set; }
+    }
+}
