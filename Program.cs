7c7bcfcc195d9d11851ac14a4cec69276f79f41d
Brady Williams <williams.cole29@yahoo.com>
2021-11-22 13:29:57 -0600

    Finished Phase 5

--- Program.cs
+++ Program.cs
@@ -7,13 +7,11 @@ namespace plan_your_heist
     {
         public static List<TeamMember> Team = new List<TeamMember>();
         public static int BankDifficultyLevel = 100;
-        public static int TeamSkillLevel;
+        public static int TeamSkillLevel = 0;
         public static int HeistLuck;
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
-            HeistLuck = new Random().Next(-10, 10);
-            BankDifficultyLevel += HeistLuck;
             AddTeamMember();
         }
 
@@ -24,18 +22,27 @@ namespace plan_your_heist
             member1.Name = Console.ReadLine();
             if (string.IsNullOrWhiteSpace(member1.Name))
             {
-                Console.WriteLine($"Team Skill Level: {TeamSkillLevel}");
-                Console.WriteLine("Bank Difficulty Level: {0}", BankDifficultyLevel);
-                if (TeamSkillLevel >= BankDifficultyLevel)
+
+                Console.Write("How many times do you want this to run? ");
+                int trialRuns = int.Parse(Console.ReadLine());
+                for (int i = 0; i < trialRuns; i++)
                 {
-                    Console.WriteLine("You succesfully robbed the bank!");
-                    Environment.Exit(0);
+                    HeistLuck = new Random().Next(-10, 10);
+                    BankDifficultyLevel += HeistLuck;
+                    Console.WriteLine($"Team Skill Level: {TeamSkillLevel}");
+                    Console.WriteLine("Bank Difficulty Level: {0}", BankDifficultyLevel);
+                    if (TeamSkillLevel >= BankDifficultyLevel)
+                    {
+                        Console.WriteLine("You succesfully robbed the bank!");
+                        // Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Uh oh! You and your team got caught :(");
+                        // Environment.Exit(0);
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Uh oh! You and your team got caught :(");
-                    Environment.Exit(0);
-                }
+                Environment.Exit(0);
             }
             Console.Write($"Enter {member1.Name}'s skill level: ");
             member1.SkillLevel = int.Parse(Console.ReadLine());
@@ -44,10 +51,9 @@ namespace plan_your_heist
             member1.CourageFactor = decimal.Parse(Console.ReadLine());
             Team.Add(member1);
             Console.WriteLine("Team member added!");
-            foreach (TeamMember member in Team)
-            {
-                TeamSkillLevel += member.SkillLevel;
-            }
+
+            TeamSkillLevel += member1.SkillLevel;
+
             AddTeamMember();
         }
     }
