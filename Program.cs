596723eb2ebd3ed54fd458ba78cbb84fa033196a
Samuel <samuelcatferrell@gmail.com>
2021-11-22 10:31:54 -0800

    Phase 2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace plan_your_heist
 {
     class Program
     {
+        public static List<TeamMember> Team = new List<TeamMember>();
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
-
             TeamMember member1 = new TeamMember();
-
             Console.Write("Enter team member name: ");
             member1.Name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(member1.Name))
+            {
+                Console.WriteLine("Done!");
+                Environment.Exit(0);
+            }
 
             Console.Write($"Enter {member1.Name}'s skill level: ");
             member1.SkillLevel = int.Parse(Console.ReadLine());
 
             Console.Write($"Enter {member1.Name}'s courage factor: ");
             member1.CourageFactor = decimal.Parse(Console.ReadLine());
-
-            Console.WriteLine($@"
-                Member Name: {member1.Name}
-                Member Skill Level: {member1.SkillLevel}
-                Member Courage Factor: {member1.CourageFactor}
-            ");
+            Team.Add(member1);
+            Console.WriteLine($"There are {Team.Count} team members in your team.");
+            foreach (TeamMember member in Team)
+            {
+                Console.WriteLine($@"
+                    Member Name: {member.Name}
+                    Member Skill Level: {member.SkillLevel}
+                    Member Courage Factor: {member.CourageFactor}
+                ");
+            }
+            member1 = null;
+            Main(new string[] { });
         }
     }
 }
